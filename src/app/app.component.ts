ae7ad807f580b29762f439dc5c79fb31c0b255e1
matthew twyford <matthewtwyford@googlemail.com>
2018-03-09 16:37:49 -0500

    Signed-off-by: matthew twyford <matthewtwyford@googlemail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   
+  
+  tester = [
+    {text: 'One', cols: 2, rows: 1, color: 'red'},
+    {text: 'Two', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+  
+  
   tiles = [
     {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
     {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
