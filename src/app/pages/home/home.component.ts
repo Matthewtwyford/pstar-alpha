8a77d68b5c5b3c2a3113d8902488b65370bd6b45
matthew twyford <matthewtwyford@googlemail.com>
2018-03-10 21:50:00 -0500

    Signed-off-by: matthew twyford <matthewtwyford@googlemail.com>

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -50,8 +50,8 @@ export class HomeComponent {
 
    tiles = [
     {text: 'Take the Test', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Revision', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Categories', cols: 1, rows: 1, color: 'lightpink'},
     {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
   ];
   
