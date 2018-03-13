6b7cd5a20e006b68bb540350287437f94eeb8195
matthew twyford <matthewtwyford@googlemail.com>
2018-03-12 20:09:57 -0400

    Signed-off-by: matthew twyford <matthewtwyford@googlemail.com>

--- src/app/pages/abbreviations/abbreviations.component.ts
+++ src/app/pages/abbreviations/abbreviations.component.ts
@@ -8,12 +8,13 @@ import { Component } from '@angular/core';
 export class AbbreviationsComponent {
   title = 'abbreviatsion';
 
-  abbreviations: any = {
-    
+  abbreviations: any = [
+
     "A" :[
        { attr: 'AAE', text: 'Above Aerodrome level'},
        { attr: 'ADIZ', text: 'Air Defence Identicationm Zone'},
       ],
+    
      "B" :[
        { attr: 'BB', text: 'VVVAbove Aerodrome level'},
        { attr: 'BB', text: 'CCCCAir Defence Identicationm Zone'},
@@ -30,6 +31,7 @@ export class AbbreviationsComponent {
        { attr: 'DD', text: 'VVVAbove Aerodrome level'},
        { attr: 'DD', text: 'CCCCAir Defence Identicationm Zone'},
       ],
- };
+
+  ];
   
 }
