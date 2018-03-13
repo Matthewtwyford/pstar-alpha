f9d71f5ee3a8e01378500529c63dd9bfea140072
matthew twyford <matthewtwyford@googlemail.com>
2018-03-13 00:02:27 -0400

    Signed-off-by: matthew twyford <matthewtwyford@googlemail.com>

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,15 +3,15 @@ import { Component } from '@angular/core';
 @Component({
 selector: 'app-home',
 template: `
- <mat-grid-list cols="4" rowHeight="300px">
-    <mat-grid-tile
-        *ngFor="let tile of tiles"
-        [colspan]="tile.cols"
-        [rowspan]="tile.rows"
-        [style.background]="tile.color">
-      {{tile.text}}
-    </mat-grid-tile>
-  </mat-grid-list>
+<mat-grid-list cols="4" rowHeight="350px">
+  <mat-grid-tile
+      *ngFor="let tile of tiles"
+      [colspan]="tile.cols"
+      [rowspan]="tile.rows"
+      [style.background]="tile.color">
+    <h2>{{tile.text}}</h2>
+  </mat-grid-tile>
+</mat-grid-list>
 `,
    styleUrls: ['./home.component.css']
 })
@@ -19,10 +19,10 @@ export class HomeComponent {
   title = 'home';
 
    tiles = [
-    {text: 'Take the Test', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Revision', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Categories', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Disclaimer', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'Take the Test', cols: 3, rows: 1, color: 'lightblue' },
+    {text: 'Revision',      cols: 1, rows: 2, color: 'lightgreen' },
+    {text: 'Categories',    cols: 1, rows: 1, color: 'lightpink' },
+    {text: 'Disclaimer',    cols: 2, rows: 1, color: '#DDBDF1' },
   ];
 
 }
