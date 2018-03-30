5280b8c023896e0f10b6e54f2406a59a02d3567c
matthew twyford <matthewtwyford@googlemail.com>
2018-03-30 18:32:56 -0400

    Signed-off-by: matthew twyford <matthewtwyford@googlemail.com>

--- src/app/pages/abbreviations/abbreviations.component.ts
+++ src/app/pages/abbreviations/abbreviations.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 @Component({
 selector: 'app-abbreviations',
 template: `
-<app-pagetitle [pageTitle]="title"></app-pagetitle>
+<app-pagetitle [pageTitle]="abbreviations.title"></app-pagetitle>
 
-
-<mat-list *ngFor="let letter of abbreviations">
-  <h2 mat-subheader>narm  {{ letter }}</h2>
+  {{ abbreviations.letters |  json }}
+  
+<!--
+<mat-list *ngFor="let letter of abbreviations.letters">
+  <h2 mat-subheader> {{ letterTitle }}</h2>
   <mat-list-item *ngFor="let item of letter">
     <mat-icon mat-list-icon>folder</mat-icon>
     <h4 mat-line>{{ letter.attr }}</h4>
@@ -15,38 +17,51 @@ template: `
   </mat-list-item>
   <mat-divider></mat-divider>
 </mat-list>
+-->
 
-`,
-style : []
+
+  `,
+  style : []
 })
 export class AbbreviationsComponent {
   title = 'Abbreviations';
 
-
-  abbreviations: any = [
-
-    "A" : [
-       { attr: 'AAE', text: 'Above Aerodrome level'},
-       { attr: 'ADIZ', text: 'Air Defence Identicationm Zone'},
-      ],
+ myObj = {
+    "name":"John",
+    "age":30,
+    "cars": [
+        
+        { "name":"Ford", 
+          "models":[ 
+            "Fiesta", 
+            "Focus", 
+            "Mustang" 
+          ] 
+        },
+        
+        { "name":"BMW", "models":[ "320", "X3", "X5" ] },
+        { "name":"Fiat", "models":[ "500", "Panda" ] }
+    ]
+ } 
+  
+  
+  abbreviations = {
     
-     "B" : [
-       { attr: 'BB', text: 'VVVAbove Aerodrome level'},
-       { attr: 'BB', text: 'CCCCAir Defence Identicationm Zone'},
-      ],
-     "C" : [
-       { attr: 'CC', text: 'VVVAbove Aerodrome level'},
-       { attr: 'CC', text: 'CCCCAir Defence Identicationm Zone'},
-      ],
-     "D" : [
-       { attr: 'DD', text: 'VVVAbove Aerodrome level'},
-       { attr: 'DD', text: 'CCCCAir Defence Identicationm Zone'},
-      ],
-     "E" : [
-       { attr: 'DD', text: 'VVVAbove Aerodrome level'},
-       { attr: 'DD', text: 'CCCCAir Defence Identicationm Zone'},
-      ],
-  ];
+    "title" : "Abbreviations",
+    
+    "letters" : [
+        
+        "letterTitle" : "A" ,
+    
+         { attr: 'AAE', text: 'Above Aerodrome level'},
+         { attr: 'ADIZ', text: 'Air Defence Identicationm Zone'},
+    
+    
+    ]
+    
+    
+    
+  };
  
   
 }
