3f34ff549be32c2974d0140ed0701c12de217044
matthew twyford <matthewtwyford@googlemail.com>
2018-03-31 15:05:57 -0400

    Signed-off-by: matthew twyford <matthewtwyford@googlemail.com>

--- src/app/pages/study/study.component.ts
+++ src/app/pages/study/study.component.ts
@@ -3,15 +3,47 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
 template:`
+
+  
+  
  <app-pagetitle [pageTitle]="page['title']"></app-pagetitle>
 {{ catid }}
 
+
+
+<mat-grid-list cols="12" rowHeight="400px" gutterSize='20px'>
+  <mat-grid-tile *ngFor="let questions of page.questions"
+     [colspan]="12"
+     [rowspan]="1"
+     [style.background]="'#F4F4F4'">
+
+    <mat-card class="card">
+      <mat-card-header>
+        <div mat-card-avatar class="example-header-image"></div>
+          <mat-card-title> <h2>{{ questions.id }}  {{ questions.question }}</h2></mat-card-title>
+        </mat-card-header>
+
+        <mat-card-content>
+           <mat-list role="list">
+              <mat-list-item role="listitem" *ngFor="let option of questions.options">
+                  {{ option.text }} <span [ngClass]="{'bold' :  option.correct  }"> {{ option.correct }} </span>
+                  <mat-divider></mat-divider>
+              </mat-list-item>
+            </mat-list>
+        </mat-card-content>
+  
+     </mat-card>
+    
+   </mat-grid-tile>
+ </mat-grid-list>
+  
+  
 
 <mat-card *ngFor="let questions of page.questions">
    <h2>{{ questions.id }}  {{ questions.question }}</h2>   
    <mat-list role="list">
     <mat-list-item role="listitem" *ngFor="let option of questions.options">
-        {{ option.text }} : {{ option.correct }} 
+        {{ option.text }} <span [ngClass]="{'bold' :  option.correct  }"> {{ option.correct }} </span>
         <mat-divider></mat-divider>
     </mat-list-item>
   </mat-list>
@@ -26,12 +58,12 @@ export class StudyComponent implements OnInit, OnDestroy {
 
   
   page = {
-    "catid" : 1,
-    "title" : 'Collision Avoidance',
-    "questions" : [
+    'catid' : 1,
+    'title' : 'Collision Avoidance',
+    'questions' : [
        {
-        "id" : 1,
-        "question" : "Which statement is true with regard to aircraft converging at approximately the same altitude?",
+        'id' : 1,
+        'question' : 'Which statement is true with regard to aircraft converging at approximately the same altitude?',
         options : [
           { text : 'A jet airliner has the right of way over all other aircraft', correct: false  },
           { text : 'An aircraft towing objects has the right of way over all power-driven heavier-than-air aircraft.', correct: true },
@@ -40,8 +72,8 @@ export class StudyComponent implements OnInit, OnDestroy {
           ]
         },
        {
-        "id" : 2,
-        "question" : "When two aircraft are converging at approximately the same altitude",
+        'id' : 2,
+        'question' : 'When two aircraft are converging at approximately the same altitude',
         options : [
           { text : 'both aircraft shall alter heading to the left.', correct: false  },
           { text : 'the aircraft on the right shall avoid the other by descending.', correct: false },
@@ -50,16 +82,87 @@ export class StudyComponent implements OnInit, OnDestroy {
         ]
        },
        {
-        "id" : 3,
-        "question" : "When two aircraft are converging at approximately the same altitude, which statement applies?",
-        options : [
+        'id' : 3,
+        'question' : 'When two aircraft are converging at approximately the same altitude, which statement applies?',
+         options : [
           { text : 'Gliders shall give way to helicopters.', correct: false  },
           { text : 'Aeroplanes shall give way to power-driven heavier-than-air aircraft.', correct: false },
           { text : 'Gliders shall give way to aeroplanes.', correct: false },
           { text : 'Power-driven heavier-than-air aircraft shall give way to gliders.', correct: true }
         ]
        },
-    
+       {
+        'id' : 4,
+        'question' : 'When two aircraft are converging at approximately the same altitude, which statement applies?',
+        options : [
+          { text : 'Gliders shall give way to helicopters.', correct: false  },
+          { text : 'Aeroplanes shall give way to helicopters.', correct: false },
+          { text : 'Helicopters shall give way to aeroplanes.', correct: false },
+          { text : 'Helicopters shall give way to gliders.', correct: true }
+        ]
+       },
+       {
+        'id' : 5,
+        'question' : 'When two aircraft are converging at approximately the same altitude, which statement applies?',
+        options : [
+          { text : 'Gliders shall give way to helicopters.', correct: false  },
+          { text : 'Aeroplanes shall give way to helicopters.', correct: false },
+          { text : 'Helicopters shall give way to aeroplanes.', correct: false },
+          { text : 'Gliders shall give way to balloons.', correct: true }
+        ]
+       },
+       {
+        'id' : 6,
+        'question' : 'When converging at approximately the same altitude',
+        options : [
+          { text : 'balloons shall give way to hang gliders.', correct: false  },
+          { text : 'aeroplanes towing gliders shall give way to balloons.', correct: true },
+          { text : 'balloons shall give way to gliders.', correct: false },
+          { text : 'balloons shall give way to airships.', correct: false }
+        ]
+       },       
+       {
+        'id' : 7,
+        'question' : 'When two power-driven heavier-than-air aircraft are converging at approximately the same altitude,',
+        options : [
+          { text : 'the one on the left has the right of way.', correct: false  },
+          { text : 'both shall alter heading to the left.', correct: false },
+          { text : 'the one on the right has the right of way.', correct: true },
+          { text : 'the one on the right shall give way by descending.', correct: false }
+        ]
+       },
+      {
+        'id' : 8,
+        'question' : 'When two aircraft are approaching head-on or approximately so and there is danger of collision, each pilot shall',
+        options : [
+          { text : 'decrease airspeed.', correct: false  },
+          { text : 'increase airspeed.', correct: false },
+          { text : 'alter heading to the right.', correct: true },
+          { text : 'alter heading to the left.', correct: false }
+        ]
+       },
+       {
+        'id' : 9,
+        'question' : 'When overtaking an aircraft at your 12 oclock position, at your altitude, you should',
+        options : [
+          { text : 'climb.', correct: false  },
+          { text : 'descend.', correct: false },
+          { text : 'alter heading to the right.', correct: true },
+          { text : 'alter heading to the left.', correct: false }
+        ]
+       },
+       {
+        'id' : 10,
+        'question' : 'Two aircraft are on approach to land, the aircraft at the higher altitude shall',
+        options : [
+          { text : 'have the right of way.', correct: false  },
+          { text : 'overtake the lower aircraft on the left..', correct: false },
+          { text : 'give way', correct: true },
+          { text : 'complete a 360° turn to the right.', correct: false }
+        ]
+       }
+       
+     
     ]
 
  };
